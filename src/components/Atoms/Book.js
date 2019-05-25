823d007bc6e209d6a9422f34ff4b1032f2f486ee
Sriram Sureshkumar <sriram.sureshkumar@viacomcontractor.com>
2019-05-25 18:56:09 +0530

    updated layout

--- src/components/Atoms/Book.js
+++ src/components/Atoms/Book.js
@@ -2,25 +2,21 @@ import React from 'react';
 
 const Book = () => (
   <div className="row">
-    <div className="col s12 m2">
-      <div className="card horizontal">
+    <div className="col s12 m3">
+      <div className="card">
         <div className="card-content">
-          <img
-            style={{ height: '180px', width: '120px' }}
-            src="https://books.google.com/books/content?id=m3r_CgAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
-            alt="book"
-          />
-          <span className="card-title">Adaptive Web Design</span>
-          <p>
-            I am a very simple card. I am good at containing small bits of
-            information. I am convenient because I require little markup to use
-            effectively.
-          </p>
-        </div>
-        <div className="card-action">
-          <button className="btn-floating halfway-fab waves-effect waves-light red">
-            <i className="material-icons">add</i>
-          </button>
+          <div className="row">
+            <div className="col s3 m3">
+              <img
+                style={{ height: '120px', width: '80px' }}
+                src="https://books.google.com/books/content?id=m3r_CgAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+                alt="book"
+              />
+            </div>
+            <div className="col s9 m9">
+              <span>Adaptive Web Design</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
